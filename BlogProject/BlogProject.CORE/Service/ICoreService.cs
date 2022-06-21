a41e1eaf37c533c2dc3d3b1c0112e38a243f5f13
Fatma-Eraslan <fatoseraslan275@gmail.com>
2022-06-21 19:58:36 +0300

    Blog project first upload.

---
+++ BlogProject/BlogProject.CORE/Service/ICoreService.cs
@@ -0,0 +1,29 @@
+﻿using BlogProject.CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.CORE.Service
+{
+    public interface ICoreService<T> where T : CoreEntity
+    {
+        bool Add(T item);
+        bool Add(List<T> items);
+        bool Update(T item);
+        bool Remove(T item);
+        bool Remove(Guid id);
+        bool RemoveAll(Expression<Func<T,bool>> exp);//linq sorgusuyla işlemi..
+        T GetByID(Guid id);
+        T GetByDefault(Expression<Func<T, bool>> exp);
+        List<T> GetDefault(Expression<Func<T, bool>> exp);
+        List<T> GetActive();
+        List<T> GetAll();
+        bool Activate(Guid id);//aktifleştir
+        bool Any(Expression<Func<T, bool>> exp); //sorgu sonucu var mı?
+        int Save();
+        
+    }
+}
---
+++ BlogProject/BlogProject.MODEL/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using BlogProject.CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.MODEL.Entities
+{
+    public class Category:CoreEntity
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        //Navigation Property
+        //Bir kategorinin birden fazla postu olabilir
+        public virtual List<Post> Posts { get; set; }
+    }
+}
