409991483d92adce7a10ff537b8d041826aef81b
Fatma-Eraslan <fatoseraslan275@gmail.com>
2022-06-23 13:42:28 +0300

    contex class and servives

--- BlogProject/BlogProject.UI/Startup.cs
+++ BlogProject/BlogProject.UI/Startup.cs
@@ -1,5 +1,7 @@
+using BlogProject.MODEL.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,7 @@ namespace BlogProject.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<BlogContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyBlogSiteConStr")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
